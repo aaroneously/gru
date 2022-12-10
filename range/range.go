76e1f6d8ee10efd780787aa5d709ea5172c8d78f
Aaron Lewing <alewing@gmail.com>
2022-12-10 12:37:08 -0500

    Update comments on range.go

--- range/range.go
+++ range/range.go
@@ -9,9 +9,9 @@ variety of data structures.
 func main() {
 
 	/*
-		    Here we use range to sum the
-			numbers in a slice. Arrays work
-			like this too.
+	Here we use range to sum the
+	numbers in a slice. Arrays work
+	like this too.
 	*/
 	nums := []int{2, 3, 4}
 	sum := 0
@@ -21,12 +21,12 @@ func main() {
 	fmt.Println("sum:", sum)
 
 	/*
-	   range on arrays and slices provides
-	   both the index and value for each entry.
-	   Above we didn’t need the index, so we
-	   ignored it with the blank identifier _.
-	   Sometimes we actually want the indexes
-	   though.
+	range on arrays and slices provides
+	both the index and value for each entry.
+	Above we didn’t need the index, so we
+	ignored it with the blank identifier _.
+	Sometimes we actually want the indexes
+	though.
 	*/
 	for i, num := range nums {
 		if num == 3 {
@@ -40,17 +40,18 @@ func main() {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
-	/* range can also iterate over just the keys of
-	   a map.
+	/* 
+	range can also iterate over just the keys of
+	a map.
 	*/
 	for k := range kvs {
 		fmt.Println("key:", k)
 	}
 
 	/*
-	   range on strings iterates over Unicode code
-	   points. The first value is the starting byte
-	   index of the rune and the second the rune itself.
+	range on strings iterates over Unicode code
+	points. The first value is the starting byte
+	index of the rune and the second the rune itself.
 	*/
 	for i, c := range "go" {
 		fmt.Println(i, c)
