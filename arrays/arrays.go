db01a848276e17e99e6d5d446b66eca4801c62ff
Aaron Lewing <alewing@gmail.com>
2022-12-23 23:33:03 -0500

    Another comment updated for arrays.go

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -33,9 +33,17 @@ func main() {
 	*/
 	fmt.Println("len:", len(a))
 
+	/*
+	Use this syntax to declare and initialize an array 
+	in one line.
+	*/
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
+	/*
+	Array types are one-dimensional, but you can compose 
+	types to build multi-dimensional data structures.
+	*/
 	var twoD [2][3]int
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 3; j++ {
