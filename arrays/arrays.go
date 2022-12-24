4fee0f74812cb1ae3de4fc415d970ae373c104d4
Aaron Lewing <alewing@gmail.com>
2022-12-23 23:31:22 -0500

    Updated comments on array.go

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -19,10 +19,18 @@ func main() {
 	var a [5]int
 	fmt.Println("emp:", a)
 
+	/*
+	We can set a value at an index using the 
+	array[index] = value syntax, and get a value with 
+	array[index].
+	*/
 	a[4] = 100
 	fmt.Println("set:", a)
 	fmt.Println("get:", a[4])
 
+	/*
+	The builtin len returns the length of an array.
+	*/
 	fmt.Println("len:", len(a))
 
 	b := [5]int{1, 2, 3, 4, 5}
